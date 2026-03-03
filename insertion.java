bf82574100bd40f4b50087349587780697e1831e
Ashwin5304 <ashwin.aithal16@gmail.com>
2026-03-03 11:10:31 +0530

    Ashwin

--- insertion.java
+++ insertion.java
@@ -4,3 +4,11 @@ public class InsertionSortExample {
         for (int i = 1; i < arr.length; i++) {
             int key = arr[i];      // Current element
             int j = i - 1;
+while (j >= 0 && arr[j] > key) {
+arr[j + 1] = arr[j];
+j--;
+}
+
+arr[j + 1] = key; // Place key in correct position
+}
+}
